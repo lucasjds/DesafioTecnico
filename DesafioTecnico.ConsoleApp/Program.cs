19af31edc2250da1331d247228cdbd5ddb3eca2e
lucas <lucassouza690@gmail.com>
2021-02-03 17:17:04 -0300

    Crianção do método obter divisores

--- DesafioTecnico.ConsoleApp/Program.cs
+++ DesafioTecnico.ConsoleApp/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DesafioTecnico.ConsoleApp
 {
-  class Program
+  public class Program
   {
+
+    public static List<int> ObtemDivisores(int numero)
+    {
+      List<int> divisores = new List<int>();
+      for (int i = 1; i <= Math.Sqrt(numero); i++)
+      {
+        if (numero % i == 0)
+        {
+          divisores.Add(i);
+          if ((numero / i) != i )
+            divisores.Add(numero / i);
+        }
+      }
+      divisores.Sort();
+      return divisores;
+    }
+
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      var result = ObtemDivisores(100);
+      foreach (int number in result)
+      {
+        Console.WriteLine(number);
+      }
+
     }
   }
 }
